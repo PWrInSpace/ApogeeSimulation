9dc8c180220d5920e781456f5c884ca6d231378d
Krzysztof Gliwinski <259282@student.pwr.edu.pl>
2021-11-18 10:01:56 +0100

    Added drag curves (Cd) for R2s and Air Breaker

--- data_processing/cd(machnum)_interpolation.py
+++ data_processing/cd(machnum)_interpolation.py
@@ -2,21 +2,24 @@ import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline as interPolate
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt('/media/krzysztof/dysk 2/PWr/Poliwrocket/Praca/Funkcja zamykająca zawór w silniku/Cd(machnum).csv', delimiter = ',')
-func = interPolate(data[1:,0], data[1:,1])
+CdFile = 'air_breaker_drag.csv'
+
+
+data = np.genfromtxt(CdFile, delimiter = ' ')
+func = interPolate(data[0:,0], data[0:,2])
 
 # matplotlib plot
-x = np.linspace(0, 1, num=1000, endpoint=True)
-plt.plot(data[1:35,0], data[1:35,1], 'o')
+x = np.linspace(0, 3, num=1000, endpoint=True)
+plt.plot(data[0:100,0], data[0:100,2], 'o')
 plt.plot(x, func(x), '-')
 plt.legend(['Data points', 'Interpolated'])
 plt.xlabel('Mach number')
 plt.ylabel('Cd')
 plt.show()
 
-savefile = open("/media/krzysztof/dysk 2/PWr/Poliwrocket/Praca/Funkcja zamykająca zawór w silniku/data.txt", "w")
+savefile = open("../sim/data/air_breaker_data.txt", "w")
 
-for i in range(101):
+for i in range(301):
     Cd = func(i/100.0)
     savefile.write(str(Cd))
     savefile.write("\n")
